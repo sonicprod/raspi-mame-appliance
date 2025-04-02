a6cda696ab87b9b5047bef27a9dec09f451fd693
Benoit <65125331+sonicprod@users.noreply.github.com>
2025-04-02 17:23:52 -0400

    Create RaspiOSSystemConfig.sh

---
+++ _staging/RaspiOSSystemConfig.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This script configure base system-wide OS settings
+GITHUB_SRCBASE=https://github.com/sonicprod/raspi-mame-appliance
+
+git clone ............
+# Config template (to be customized)...
+# Config.ExportPublicImage.cfg
