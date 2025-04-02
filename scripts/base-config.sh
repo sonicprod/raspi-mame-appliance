cc44cb373bd27ec123f7e686ce9d259422319ce7
Benoit <65125331+sonicprod@users.noreply.github.com>
2025-04-02 17:21:50 -0400

    Delete scripts/base-config.sh

--- scripts/base-config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This script configure base system-wide OS settings
-GITHUB_SRCBASE=https://github.com/sonicprod/raspi-mame-appliance
-
-
-# Fetch Custom_Settings.txt template (to be customized)...
-wget $GITHUB_SRCBASE/Custom_Settings.txt
-
