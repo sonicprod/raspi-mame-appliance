0d0a54f975ba14a625eccda0f4baed7859aad515
Benoit <65125331+sonicprod@users.noreply.github.com>
2025-04-02 21:42:53 -0400

    Create FinalizeRaspiOSImageFile.sh

---
+++ POST-PROCESS/FinalizeRaspiOSImageFile.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ ! $1 ]; then
+    echo Usage: $0 VER [zero]
+    echo '  Where VER is the 4-digit version number of MAME to update (for example: 0224).'
+    echo '  Where zero overwrite the free space with zeros to optimize compression.'
+    exit
+fi
+
+# IMGNAME=rpi4b.raspios.mame-$1.appliance.img
+IMGNAME=rpi4b.raspios.mame-$1.appliance.fe-edition.img
+SCRIPTPATH=${0%/*}
+
+if [ ! -f $IMGNAME ]; then
+    echo $IMGNAME does not exist!
+    exit
+fi
+
+if [ -f $IMGNAME.gz ]; then
+    rm $IMGNAME.gz
+fi
+
+# Écrasement de l'espace libre de rootfs avec des zéros
+if [ "$2" = "zero" ]; then
+    $SCRIPTPATH/mount.part.rpi4b.raspios.mame.sh $1 rootfs zero
+fi
+
+echo -n "Compressing $IMGNAME..."
+gzip -9 -k $IMGNAME
+echo
+
+ls -la $IMGNAME $IMGNAME.gz
