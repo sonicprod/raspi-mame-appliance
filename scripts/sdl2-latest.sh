f3cf5281aa152a5aebd017b6e3722c3ec7a80e92
Benoit <65125331+sonicprod@users.noreply.github.com>
2025-04-03 22:12:29 -0400

    Create sdl2-latest.sh

---
+++ scripts/sdl2-latest.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# This script build the latest SDL2 version without X11 dependency.
+
+SDLVERSION=2.30.12
+TTFVERSION=2.24.0
+
+if [ "$(command -v sdl2-config)" ] && [ "$(sdl2-config --version)" == "$SDLVERSION" ]; then
+  echo "SDL2 is already at the latest version ($SDLVERSION)."
+  exit
+else
+  if [ "${1,,}" != "nodep" ]; then
+    echo Installing SDL2 dependencies...
+    sudo apt-get install libfreetype6-dev libdrm-dev libgbm-dev libudev-dev libdbus-1-dev libasound2-dev liblzma-dev libjpeg-dev libtiff-dev libwebp-dev -y
+    echo OpenGL ES 2 dependencies...
+    sudo apt-get install libgles2-mesa-dev -y
+  fi
+  echo Build dependencies...
+  sudo apt-get install build-essential -y
+  cd ~
+  echo Buiding SDL2 $SDLVERSION...
+  # Based from "Compile SDL2 from source"
+  # https://github.com/midwan/amiberry/wiki/Compile-SDL2-from-source
+  wget https://libsdl.org/release/SDL2-${SDLVERSION}.zip
+  unzip SDL2-${SDLVERSION}.zip
+  rm SDL2-${SDLVERSION}.zip
+  cd SDL2-${SDLVERSION}
+  ./configure --disable-video-opengl --disable-video-opengles1 --disable-video-x11 --disable-pulseaudio --disable-esd --disable-video-wayland --disable-video-rpi --disable-video-vulkan --enable-video-kmsdrm --enable-video-opengles2 --enable-alsa --disable-joystick-virtual --enable-arm-neon --enable-arm-simd
+
+  make -j $(nproc)
+  sudo make install
+
+  # SDL2_ttf
+  wget https://libsdl.org/projects/SDL_ttf/release/SDL2_ttf-${TTFVERSION}.zip
+  unzip SDL2_ttf-${TTFVERSION}.zip
+  rm SDL2_ttf-${TTFVERSION}.zip
+  cd SDL2_ttf-${TTFVERSION}
+  ./configure
+  make -j $(nproc)
+  sudo make install
+  sudo ldconfig -v
+
+  cd ~
+  sudo rm -R SDL2-${VERSION}
+  sudo rm -R SDL2_ttf-${TTFVERSION}
+  sudo apt-get remove build-essential -y
+fi
+
+sudo ldconfig
