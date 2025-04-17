e39d444f1aff0ea216a30dec96600190811ab590
Benoit <65125331+sonicprod@users.noreply.github.com>
2025-04-17 08:06:07 -0400

    Update FinalizeRaspiOSImageFile.sh

--- POST-PROCESS/FinalizeRaspiOSImageFile.sh
+++ POST-PROCESS/FinalizeRaspiOSImageFile.sh
@@ -4,7 +4,7 @@
 # - Add the init=/usr/lib/raspi-config/Expand-Data-Partition.sh to $BOOTDIR/cmdline.txt
 # - Mount the rootfs partition
 # - Remove (rm) the bootstrap.service unit file and associated links (if applicable)
-# - Cleanup command history, rm ~/.sudo_as_admin_successful ~/.lesshst
+# - Cleanup command history, journalctl, rm ~/.sudo_as_admin_successful ~/.lesshst
 # - Fill the free space with zeros (/dev/zero) to make a more compact .img file, once compressed
 # - Compress the image file with gzip
 # - Print a message to inform that the SD card is ready to be published
