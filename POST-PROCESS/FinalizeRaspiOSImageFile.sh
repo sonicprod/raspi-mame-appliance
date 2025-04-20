1d42029163f0e41ae41e1dd064affd57f42519ce
Benoit <65125331+sonicprod@users.noreply.github.com>
2025-04-20 13:53:03 -0400

    Update FinalizeRaspiOSImageFile.sh

--- POST-PROCESS/FinalizeRaspiOSImageFile.sh
+++ POST-PROCESS/FinalizeRaspiOSImageFile.sh
@@ -4,6 +4,7 @@
 # - Add the init=/usr/lib/raspi-config/Expand-Data-Partition.sh to $BOOTDIR/cmdline.txt
 # - Mount the rootfs partition
 # - Remove (rm) the bootstrap.service unit file and associated links (if applicable)
+# - Comment Storage= in /etc/systemd/journald.conf
 # - Cleanup command history, journalctl, rm ~/.sudo_as_admin_successful ~/.lesshst
 # - Fill the free space with zeros (/dev/zero) to make a more compact .img file, once compressed
 # - Compress the image file with gzip
