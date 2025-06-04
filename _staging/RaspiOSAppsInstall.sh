d3a48d81cddb2943ebe018dfe9038c398a138d44
Benoit <65125331+sonicprod@users.noreply.github.com>
2025-06-03 21:41:06 -0400

    Update RaspiOSAppsInstall.sh

--- _staging/RaspiOSAppsInstall.sh
+++ _staging/RaspiOSAppsInstall.sh
@@ -43,18 +43,18 @@ hi_path                   $HOME/.mame/hi
 EOF
 
 # Externalisation de dossiers additionnels au sein de ui.ini vers le profil :
-sed -i 's/cabinets_directory.*$/cabinets_directory        $HOME\/.mame\/cabinets/g' /home/pi/.mame/ui.ini
-sed -i 's/cpanels_directory.*$/cpanels_directory         $HOME\/.mame\/cpanels/g' /home/pi/.mame/ui.ini
-sed -i 's/pcbs_directory.*$/pcbs_directory            $HOME\/.mame\/pcb/g' /home/pi/.mame/ui.ini
-sed -i 's/flyers_directory.*$/flyers_directory          $HOME\/.mame\/flyers/g' /home/pi/.mame/ui.ini
-sed -i 's/historypath.*$/historypath               $HOME\/.mame\/history/g' /home/pi/.mame/ui.ini
-sed -i 's/titles_directory.*$/titles_directory          $HOME\/.mame\/titles/g' /home/pi/.mame/ui.ini
-sed -i 's/marquees_directory.*$/marquees_directory        $HOME\/.mame\/marquees/g' /home/pi/.mame/ui.ini
-sed -i 's/icons_directory.*$/icons_directory           $HOME\/.mame\/icons/g' /home/pi/.mame/ui.ini
-sed -i 's/ui_path.*$/ui_path                   $HOME\/.mame\/ui/g' /home/pi/.mame/ui.ini
+sed -i 's|cabinets_directory.*$|cabinets_directory        $HOME/.mame/cabinets|g' /home/pi/.mame/ui.ini
+sed -i 's|cpanels_directory.*$|cpanels_directory         $HOME/.mame/cpanels|g' /home/pi/.mame/ui.ini
+sed -i 's|pcbs_directory.*$|pcbs_directory            $HOME/.mame/pcb|g' /home/pi/.mame/ui.ini
+sed -i 's|flyers_directory.*$|flyers_directory          $HOME/.mame/flyers|g' /home/pi/.mame/ui.ini
+sed -i 's|historypath.*$|historypath               $HOME/.mame/history|g' /home/pi/.mame/ui.ini
+sed -i 's|titles_directory.*$|titles_directory          $HOME/.mame/titles|g' /home/pi/.mame/ui.ini
+sed -i 's|marquees_directory.*$|marquees_directory        $HOME/.mame/marquees|g' /home/pi/.mame/ui.ini
+sed -i 's|icons_directory.*$|icons_directory           $HOME/.mame/icons|g' /home/pi/.mame/ui.ini
+sed -i 's|ui_path.*$|ui_path                   $HOME/.mame/ui|g' /home/pi/.mame/ui.ini
 
 # Put MAME UI in Available ROMs mode
-sed -i 's/last_used_filter.*$/last_used_filter          Available' /home/pi/.mame/ui.ini
+sed -i 's/last_used_filter.*$/last_used_filter          Available/g' /home/pi/.mame/ui.ini
 
 # Test ROM to make sure everything is OK
 # This ROM *will* be deleted and NOT included in the final image
